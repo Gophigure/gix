a0169bc04dd9ed226c641478f49f853328aa54af
Sepruko <natsepruko@gmail.com>
2022-04-08 17:35:39 +1000

    fix(fmt): Increase rounding accuracy

--- fmt/storage.go
+++ fmt/storage.go
@@ -22,7 +22,7 @@ var (
 func Bytes(b uint64) string {
 	for i, size := range sizesSI {
 		if i > 0 && b/1000 < size {
-			val, idx := float64(b)/math.Pow(1000, float64(i)), 7*i+5
+			val, idx := float64(b)/math.Pow(1000, float64(i))-0.005, 7*i+5
 			return strconv.FormatFloat(val, 'f', 2, 64) + suffixes[idx:idx+2]
 		} else if i == 0 && b < size {
 			return strconv.FormatUint(b, 10) + suffixes[2:3]
